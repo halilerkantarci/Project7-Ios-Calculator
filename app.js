88c1b3747e126e4fd57dbac70b4b55e6f64ec723
halil <halilerk77@icloud.com>
2022-06-11 13:26:59 +0300

    .

--- app.js
+++ app.js
@@ -1,34 +1,97 @@
-//* ==================================================================
-//*                        IOS CALCULATOR
-//* ==================================================================
-
+//* =================================================
+//*                     IOS CALCULATOR
+//* =================================================
 const prevDisp = document.querySelector(".previous-display");
 const currDisp = document.querySelector(".current-display");
 
 const btnContainer = document.querySelector(".buttons-container");
 
 let currOperand = "";
-let prevOperand = "";
+let previousOperand = "";
+let operation = "";
 
-//we defined an event for buttons container
+//? Butonlari tasiyan container icin event tanimlamasi
 btnContainer.addEventListener("click", (e) => {
   if (e.target.classList.contains("num")) {
     appendNumber(e.target.textContent);
     updateDisplay();
   }
+
+  if (e.target.classList.contains("operator")) {
+    chooseOperator(e.target.textContent);
+    updateDisplay();
+  }
+
+  if (e.target.classList.contains("equal")) {
+    calculate();
+    updateDisplay();
+  }
 });
 
 const appendNumber = (num) => {
-  // eğer ilk sayı sıfırsa geri döndür
-  if (!currOperand && num === "0") return;
+  //? Eger onceden 0 girilmisse ve tekrardan 0 girilise geri don
+  if (currOperand === "0" && num === "0") return;
 
-  if (currOperand.includes(".") && num === ".") return;
+  //? Eğer ilk olarak 0 girilmisse ve sonrasinda da . haricinde baska
+  //? bir sayi girilmis ise sadece girilen yeni sayiyi degiskene aktar.
+  //? Orn: 09 => 9 , 03 => 3 , 0.1 => 0.1
+  if (currOperand === "0" && num !== ".") {
+    currOperand = num;
+    return;
+  }
 
-  // girilen sayiyi birleştirir
+  //? Eğer şu anki sayi . ise ve önceki girilen sayi . iceriyorsa geri don
+  if (num === "." && currOperand.includes(".")) return;
+
+  if (currOperand.length > 10) return;
+  //? Girilen sayilari birlestir.
   currOperand += num;
 };
 
 const updateDisplay = () => {
-  // str dönüyor
   currDisp.textContent = currOperand;
+  prevDisp.textContent = `${previousOperand} ${operation}`;
+};
+
+const chooseOperator = (op) => {
+  //? ilk sayi girisiinden sonraki islemleri gercekletir
+  if (previousOperand) {
+    calculate();
+  }
+
+  //? Degisken swapiing
+  operation = op;
+  previousOperand = currOperand;
+  currOperand = "";
+};
+
+const calculate = () => {
+  let calculation = 0;
+
+  const prev = Number(previousOperand);
+  const current = Number(currOperand);
+
+  switch (operation) {
+    case "+":
+      calculation = prev + current;
+      break;
+    case "-":
+      calculation = prev - current;
+      break;
+    case "x":
+      calculation = prev * current;
+      break;
+    case "÷":
+      calculation = prev / current;
+      break;
+    default:
+      return;
+  }
+
+  currOperand = calculation;
+
+  //! eşittir butonuna tıklandığında ekranda gözükmemesi için previousoperand ve operationı silmemiz gerekir
+
+  previousOperand = "";
+  operation = "";
 };
